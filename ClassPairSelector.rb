9987294da83ecd12816b4101b21aff31fc5f9288
Marco-John <john.throckmorton4@gmail.com>
2016-02-24 02:04:12 -0500

    Changed to Array of Class Members - Sample & Delete
    Created a Array of Individual Class Members Then Selected one with
    .sample then delete selected & repeat for Second person

--- ClassPairSelector.rb
+++ ClassPairSelector.rb
@@ -1,7 +1,12 @@
 def random_pair_selector
-    class_pairs = [["Shane", "Ed"], ["Jen", "Heather"], ["Dolly", "Aaron"], ["Rich", "John"]]
+    class_members = ["Shane", "Ed", "Jen", "Heather", "Dolly", "Aaron", "Rich", "John"]
+    
+    random_member1 = class_members.sample
+    class_members.delete(random_member1)
+    random_member2 = class_members.sample
+    class_members.delete(random_member2)
                                                          
-    puts class_pairs.sample
+    puts random_member1 + " & " + random_member2
 end
 
 random_pair_selector
