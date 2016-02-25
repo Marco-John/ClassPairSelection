48389a8592adb50b2f90e726ce504f5c8aed352a
Marco-John <john.throckmorton4@gmail.com>
2016-02-25 13:10:40 -0500

    Selector2 Trying to Output Counts

--- TestClassPairSelector.rb
+++ TestClassPairSelector.rb
@@ -4,9 +4,10 @@ require_relative "ClassPairSelector.rb"
 class TestClassPairSelector < Minitest::Test
 
     def test_number_of_class_members_equal_to_9
-        assert_equal(1, x)
+        #assert_equal(1, x)
         #assert_equal(9, number_of_class_members.count)
         #assert_equal(9, class_members.count)
+        assert_equal(9, class_members("Shane", "Ed", "Jen", "Heather", "Dolly", "Aaron", "Rich", "John", "John T"))
     end
     
 end
